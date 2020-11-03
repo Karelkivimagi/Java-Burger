e7cca8ef520197ddacfbf83c65da913f243a3782
Karel Kivimägi <karel.kivimagi@khk.ee>
2020-11-03 10:21:52 +0200

    HashMap

--- src/food/burger/Main.java
+++ src/food/burger/Main.java
@@ -1,9 +1,17 @@
 package food.burger;
 
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Main {
 
     public static void main(String[] args) {
-        Input.InputMenu();
+        Sisestus.ScannerMenuu();
+        Map lihavalik = new HashMap();
+        lihavalik.put("kana", "2");
+        lihavalik.put("veise", "3");
+        lihavalik.put("sea","4");
+
     }
 }
